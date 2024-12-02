fea048e7abfd1908ad1245f609c1d1daa6c53651
rohithaider <rohithaider@gmail.com>
2024-12-02 07:13:58 +0600

    Updating in the mongoDB

--- coffee-store-client/Coffe-Emporium/src/components/Card.jsx
+++ coffee-store-client/Coffe-Emporium/src/components/Card.jsx
@@ -1,37 +1,37 @@
 import swal from "sweetalert";
 import revalidateLoaderData from "../main";
+import { Link } from "react-router-dom";
 export default function Card({ coffee }) {
-    function handleDelete(id){
-        swal({
-            title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover this imaginary file!",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((result) => {
-            if(result){
-                fetch(`http://localhost:5000/coffee/${id}`,{
-                    method:'DELETE'
-                })
-                .then(res=>res.json())
-                .then(data=>{
-                    if(data.deletedCount>0){
-                        swal("Poof! Your imaginary file has been deleted!", {
-                            icon: "success",
-                          });
-                    }
-                })
-                revalidateLoaderData();
+  function handleDelete(id) {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this imaginary file!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((result) => {
+      if (result) {
+        fetch(`http://localhost:5000/coffee/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              swal("Poof! Your imaginary file has been deleted!", {
+                icon: "success",
+              });
             }
           });
-    }
+        revalidateLoaderData();
+      }
+    });
+  }
 
   const { _id, name, chef, supplier, taste, category, details, photo } = coffee;
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <figure className="p-4">
-        <img src={photo} alt="Movie" className="rounded-lg"/>
+        <img src={photo} alt="Movie" className="rounded-lg" />
       </figure>
       <div className="flex items-center p-4 gap-24">
         <div className="">
@@ -41,8 +41,12 @@ export default function Card({ coffee }) {
         </div>
         <div className="flex flex-col gap-2 items-end">
           <button className="btn ">Watch</button>
-          <button className="btn ">Update</button>
-          <button onClick={()=>handleDelete(_id)} className="btn bg-red-500">Delete</button>
+          <Link to={`updateCoffee/${_id}`}>
+            <button className="btn ">Edit</button>
+          </Link>
+          <button onClick={() => handleDelete(_id)} className="btn bg-red-500">
+            Delete
+          </button>
         </div>
       </div>
     </div>
--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -54,6 +54,14 @@ async function run() {
       res.send(result)
 
     })
+    //!!UPDATE
+    app.get('/coffee/:id',async(req,res)=>{
+      const id  = req.params.id;
+      const query  = {_id:new ObjectId(id)}
+      const result  = await coffees.findOne(query)
+      res.send(result)
+
+    })
 
 
     // Send a ping to confirm a successful connection
