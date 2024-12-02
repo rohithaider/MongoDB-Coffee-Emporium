dbb20526bdb55675ce40b174d676348cceb78aec
rohithaider <rohithaider@gmail.com>
2024-12-02 08:01:55 +0600

    Updating in the mongoDB and in the client

--- coffee-store-client/Coffe-Emporium/src/components/UpdateCoffee.jsx
+++ coffee-store-client/Coffe-Emporium/src/components/UpdateCoffee.jsx
@@ -1,8 +1,157 @@
+import { useLoaderData } from "react-router-dom";
+import swal from "sweetalert";
+import revalidateLoaderData from "../main";
 
 const UpdateCoffee = () => {
-  return (
-    <div>UpdateCoffee</div>
-  )
-}
+  const coffee = useLoaderData();
+  const { _id, name, chef, supplier, taste, category, details, photo } = coffee;
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const coffeeData = {};
 
-export default UpdateCoffee
+    formData.forEach((value, key) => {
+      coffeeData[key] = value;
+    });
+
+    fetch(`http://localhost:5000/coffee/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(coffeeData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount>0) {
+          swal("Congratulations", "Successfully Updated! ", "success");
+        }
+        revalidateLoaderData()
+        
+      });
+  }
+  return (
+    <div className="text-center mt-10">
+      {/* form div */}
+      <form
+        className="bg-[#F4F3F0] md:w-2/3 mx-auto border p-10"
+        onSubmit={handleSubmit}
+      >
+        <h1
+          className="text-3xl font-extrabold mb-4 font-rancho"
+          style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.4)" }}
+        >
+          Update Coffee
+        </h1>
+        
+        <div>
+          <div className="flex justify-around gap-4">
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text font-bold">Name</span>
+              </div>
+              <input
+                name="name"
+                type="text"
+                defaultValue={name}
+                placeholder="Enter Coffee Name"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="form-control w-full ">
+              <div className="label">
+                <span className="label-text font-bold">Chef</span>
+              </div>
+              <input
+                name="chef"
+                type="text"
+                defaultValue={chef}
+                placeholder="Enter Coffee Chef"
+                className="input input-bordered w-full "
+              />
+            </label>
+          </div>
+          <div className="flex justify-around gap-4">
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text font-bold">Supplier</span>
+              </div>
+              <input
+                name="supplier"
+                type="text"
+                defaultValue={supplier}
+                placeholder="Enter Supplier Name"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="form-control w-full ">
+              <div className="label">
+                <span className="label-text font-bold">Taste</span>
+              </div>
+              <input
+                name="taste"
+                type="text"
+                defaultValue={taste}
+                placeholder="Enter Taste Name"
+                className="input input-bordered w-full "
+              />
+            </label>
+          </div>
+          <div className="flex justify-around gap-4">
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text font-bold">Category</span>
+              </div>
+              <input
+                name="category"
+                type="text"
+                defaultValue={category}
+                placeholder="Enter Category Name"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="form-control w-full ">
+              <div className="label">
+                <span className="label-text font-bold">Details</span>
+              </div>
+              <input
+                name="details"
+                type="text"
+                defaultValue={details}
+                placeholder="Enter Coffee Details"
+                className="input input-bordered w-full "
+              />
+            </label>
+          </div>
+          <div className="flex justify-around gap-4">
+            <label className="form-control w-full ">
+              <div className="label">
+                <span className="label-text font-bold">Photo-URL</span>
+              </div>
+              <input
+                name="photo"
+                type="text"
+                defaultValue={photo}
+                placeholder="Enter Photo URL"
+                className="input input-bordered w-full "
+              />
+            </label>
+          </div>
+          <div className="flex mt-8">
+            <label className="form-control w-full ">
+              <input
+                name="submit"
+                type="submit"
+                value="Add Coffee"
+                className="input input-bordered w-full btn bg-[#D2B48C] text-[#331A15]"
+              />
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -10,10 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nihur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nihur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,50 +25,68 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const coffees  = client.db("coffeeCollection").collection("coffee");
-    //!sending data from the API using GET method
+    const coffees = client.db("coffeeCollection").collection("coffee");
+    //!Getting all the data from the API
 
-    app.get('/coffee',async(req,res)=>{
+    app.get("/coffee", async (req, res) => {
       const cursor = coffees.find();
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // !getting data from the add coffee form and adding to mongoDB
-    app.post('/coffee',async(req,res)=>{
-        const newCoffee = req.body;
-        
-        const result  = await coffees.insertOne(newCoffee);
-        res.send(result)
-    })
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+
+      const result = await coffees.insertOne(newCoffee);
+      res.send(result);
+    });
 
     //!Delete
 
-    app.delete('/coffee/:id',async(req,res)=>{
-      const id  = req.params.id;
-      const query  = {_id:new ObjectId(id)}
-      const result  = await coffees.deleteOne(query)
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffees.deleteOne(query);
+      res.send(result);
+    });
+    //!!getting single item
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffees.findOne(query);
+      res.send(result);
+    });
+
+    //!updating a  single item
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          chef: updatedCoffee.chef,
+          supplier:updatedCoffee.supplier,
+          taste:updatedCoffee.taste,
+          category:updatedCoffee.category,
+          details:updatedCoffee.details,
+          photo:updatedCoffee.photo
+        },
+      };
+      const result = await coffees.updateOne(filter,coffee,options)
       res.send(result)
-
-    })
-    //!!UPDATE
-    app.get('/coffee/:id',async(req,res)=>{
-      const id  = req.params.id;
-      const query  = {_id:new ObjectId(id)}
-      const result  = await coffees.findOne(query)
-      res.send(result)
-
-    })
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-  } catch (error){
+  } catch (error) {
     // Ensures that the client will close when you finish/error
-    console.error("Failed to connect to MongoDB",error)
+    console.error("Failed to connect to MongoDB", error);
   }
 }
 run().catch(console.dir);
